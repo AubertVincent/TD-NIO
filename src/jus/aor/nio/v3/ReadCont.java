7b3bd8bbbc82cecf25ffbb89e8725d6358111c70
Servan42 <charlot.servan@gmail.com>
2018-02-09 12:13:02 +0100

    Fichiers du TP

---
+++ src/jus/aor/nio/v3/ReadCont.java
@@ -0,0 +1,31 @@
+package jus.aor.nio.v3;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author morat 
+ */
+public class ReadCont  extends Continuation{
+
+	// TODO
+
+	/**
+	 * @param sc
+	 */
+	public ReadCont(SocketChannel sc){
+		super(sc);
+	}
+	/**
+	 * @return the message
+	 * @throws IOException
+	 * @throws ClassNotFoundException 
+	 */
+	protected Message handleRead() throws IOException, ClassNotFoundException{
+		// TODO
+		System.out.println("NOT IMPLEMENTED YET");
+		return null;
+	}
+}
+
